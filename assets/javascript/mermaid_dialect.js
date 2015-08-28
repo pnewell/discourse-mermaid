96f9d612f02e831b92a724ea20ae2d51bdb54b43
pnewell <pnewelljr@icloud.com>
2015-08-28 10:14:49 -0400

    Create mermaid_dialect.js

---
+++ assets/javascript/mermaid_dialect.js
@@ -0,0 +1,12 @@
+(function() {
+
+Discourse.Dialect.inlineBetween({
+    start: '\[flowchart\]',
+    stop: '\[\/flowchart\]',
+    rawContents: true,
+    emitter: function(contents) {
+        return "<div class="mermaid">" + contents + "</div>";
+    }
+})
+    
+})();
