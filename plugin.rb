f16d3666c14afcd77a622f9667f1fae21ce46744
pnewell <pnewelljr@icloud.com>
2015-08-28 10:10:38 -0400

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-mermaid
+# about: Plugin for Discourse that allows the use of the Mermaid JavaScript library for flowchart creation.
+# version: 0.0.1
+# authors: Paul Newell
+# url: https://github.com/pnewell/discourse-mermaid
+
+register_asset "javascript/mermaid_dialect.js", :server_side
